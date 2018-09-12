49539e8376622dad0b115de19ec6e425ca01ff84
Steve Gilham <SteveGilham@users.noreply.github.com>
2018-09-12 16:43:14 +0100

    Supperss this until we get to that task

--- AltCover.FSApi/RenderToHtml.fs
+++ AltCover.FSApi/RenderToHtml.fs
@@ -1,12 +1,14 @@
 ﻿namespace AltCover
 
+#if TODO
 open System.Diagnostics.CodeAnalysis
 open System.Xml
 
 module RenderToHtml =
   [<SuppressMessage("Microsoft.Design", "CA1059",
     Justification="Premature abstraction")>]
-  let Action x = 
+  let Action x =
     let doc = XmlDocument()
     doc.CreateComment(x.ToString()) |> doc.AppendChild |> ignore
     doc
+#endif
