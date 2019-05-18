ffb16b736ffbc62b57107163ae1dba5333088597
Steve Gilham <SteveGilham@users.noreply.github.com>
2019-05-18 22:14:44 +0100

    Minimal stub recorder replacement so the projects build

---
+++ AltCover.Recorder/Tracer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AltCover.Recorder
+{
+    public class Tracer
+    {
+        public static string Core()
+        {
+            return String.Empty;
+        }
+    }
+}
